f9fc3b581b9eaa4401377bcb3479ee6d81676d11
Aleksandra Godun <a.godun@semrush.com>
2018-09-21 00:45:14 +0300

    Write single hash signer

--- pipeline/signer.go
+++ pipeline/signer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 )
 
@@ -29,7 +30,36 @@ func jobWorker(job job, in, out chan interface{}, wg *sync.WaitGroup) {
 	job(in, out)
 }
 
-func SingleHash() {}
+func SingleHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
+
+	for i := range in {
+		wg.Add(1)
+		go singleHashJob(i, out, wg, mu)
+	}
+}
+
+func singleHashJob(in interface{}, out chan interface{}, wg *sync.WaitGroup, mu *sync.Mutex) {
+	defer wg.Done()
+	data := strconv.Itoa(in.(int))
+
+	mu.Lock()
+	md5Data := DataSignerMd5(data)
+	mu.Unlock()
+
+	crc32Chan := make(chan string)
+	go asyncCrc32Signer(data, crc32Chan)
+
+	crc32Data := <-crc32Chan
+	crc32Md5Data := DataSignerCrc32(md5Data)
+
+	out <- crc32Data + "~" + crc32Md5Data
+}
+
+func asyncCrc32Signer(data string, out chan string) {
+	out <- DataSignerCrc32(data)
+}
 
 func main() {
 	fmt.Println("pew")
