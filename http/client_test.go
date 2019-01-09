c572ade0f4a1d0ce461074232e2c8499c015d329
Aleksandra Godun <a.godun@semrush.com>
2019-01-10 02:52:29 +0300

    Finish task

--- http/client_test.go
+++ http/client_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -12,10 +15,101 @@ import (
 	"time"
 )
 
+type UserRow struct {
+	Id        int `xml:"id"`
+	Name      string
+	Age       int    `xml:"age"`
+	About     string `xml:"about"`
+	Gender    string `xml:"gender"`
+	FirstName string `xml:"first_name"`
+	LastName  string `xml:"last_name"`
+}
+
+type Root struct {
+	Row []UserRow `xml:"row"`
+}
+
+func decode(data []byte) []UserRow {
+	input := bytes.NewReader(data)
+	decoder := xml.NewDecoder(input)
+
+	var users []UserRow
+	for {
+		token, tokenErr := decoder.Token()
+		if tokenErr == io.EOF {
+			break
+		}
+
+		if tokenErr != nil {
+			fmt.Println("error happend", tokenErr)
+			break
+		}
+
+		if token == nil {
+			fmt.Println("t is nil break")
+		}
+
+		switch token := token.(type) {
+		case xml.StartElement:
+			if token.Name.Local == "row" {
+				var user UserRow
+				err := decoder.DecodeElement(&user, &token)
+				if err != nil {
+					fmt.Println("error happend", err)
+				}
+				user.Name = user.FirstName + " " + user.LastName
+
+				users = append(users, user)
+			}
+		}
+	}
+
+	return users
+}
+
 func SearchServer(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("AccessToken") == "bad_token" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	query := r.FormValue("query")
 	if query == "timeout" {
 		time.Sleep(time.Second * 2)
+	}
+
+	if query == "internal_error" {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if query == "invalid_json" {
+		w.Write([]byte("invalid_json"))
+		return
+	}
+
+	if query == "bad_request" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if query == "bad_request_unknown" {
+		resp, _ := json.Marshal(SearchErrorResponse{"UnknownError"})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(resp)
+		return
+	}
+
+	orderField := r.FormValue("order_field")
+	if orderField == "" {
+		orderField = "Name"
+	}
+
+	if orderField != "Id" && orderField != "Age" && orderField != "Name" {
+		resp, _ := json.Marshal(SearchErrorResponse{"ErrorBadOrderField"})
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(resp)
+		return
 	}
 
 	offset, err := strconv.Atoi(r.FormValue("offset"))
@@ -36,9 +130,16 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 	}
 
 	users := decode(rawData)
-	resp, _ := json.Marshal(users[offset:(limit + offset)])
 
+	if query == "less_data" {
+		resp, _ := json.Marshal(users[offset:(limit + offset - 5)])
+		w.Write(resp)
+		return
+	}
+
+	resp, _ := json.Marshal(users[offset:(limit + offset)])
 	w.Write(resp)
+	return
 }
 
 type TestCase struct {
@@ -52,7 +153,7 @@ func TestFindUsersTimeout(t *testing.T) {
 	defer testServer.Close()
 
 	client := &SearchClient{
-		URL:         testServer.URL + "?timeout=true",
+		URL:         testServer.URL,
 		AccessToken: "good_token",
 	}
 
@@ -62,6 +163,139 @@ func TestFindUsersTimeout(t *testing.T) {
 	}
 
 	if !strings.Contains(err.Error(), "timeout for") {
+		t.Errorf("Wrong error type received, [%v]", err)
+	}
+}
+
+func TestFindUsersInternalError(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer testServer.Close()
+
+	client := &SearchClient{
+		URL:         testServer.URL,
+		AccessToken: "good_token",
+	}
+
+	resp, err := client.FindUsers(SearchRequest{Limit: 1, Query: "internal_error"})
+	if err == nil {
+		t.Errorf("Response should return error, [%v]", resp)
+	}
+
+	if !strings.Contains(err.Error(), "SearchServer fatal error") {
+		t.Errorf("Wrong error type received, [%v]", err)
+	}
+}
+
+func TestFindUsersBadRequest(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer testServer.Close()
+
+	client := &SearchClient{
+		URL:         testServer.URL,
+		AccessToken: "good_token",
+	}
+
+	resp, err := client.FindUsers(SearchRequest{Limit: 1, Query: "bad_request"})
+	if err == nil {
+		t.Errorf("Response should return error, [%v]", resp)
+	}
+
+	if !strings.Contains(err.Error(), "cant unpack error json") {
+		t.Errorf("Wrong error type received, [%v]", err)
+	}
+}
+
+func TestFindUsersBadRequestUnknown(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer testServer.Close()
+
+	client := &SearchClient{
+		URL:         testServer.URL,
+		AccessToken: "good_token",
+	}
+
+	resp, err := client.FindUsers(SearchRequest{Limit: 1, Query: "bad_request_unknown"})
+	if err == nil {
+		t.Errorf("Response should return error, [%v]", resp)
+	}
+
+	if !strings.Contains(err.Error(), "unknown bad request error") {
+		t.Errorf("Wrong error type received, [%v]", err)
+	}
+}
+
+func TestFindUsersInvalidJson(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer testServer.Close()
+
+	client := &SearchClient{
+		URL:         testServer.URL,
+		AccessToken: "good_token",
+	}
+
+	resp, err := client.FindUsers(SearchRequest{Limit: 1, Query: "invalid_json"})
+	if err == nil {
+		t.Errorf("Response should return error, [%v]", resp)
+	}
+
+	if !strings.Contains(err.Error(), "cant unpack result json") {
+		t.Errorf("Wrong error type received, [%v]", err)
+	}
+}
+
+func TestFindUsersOrderField(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer testServer.Close()
+
+	client := &SearchClient{
+		URL:         testServer.URL,
+		AccessToken: "good_token",
+	}
+
+	resp, err := client.FindUsers(SearchRequest{Limit: 1, OrderField: "order_field"})
+	if err == nil {
+		t.Errorf("Response should return error, [%v]", resp)
+	}
+
+	if !strings.Contains(err.Error(), "OrderFeld order_field invalid") {
+		t.Errorf("Wrong error type received, [%v]", err)
+	}
+}
+
+func TestFindUsersUnknownErr(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer testServer.Close()
+
+	client := &SearchClient{
+		URL:         "http://",
+		AccessToken: "good_token",
+	}
+
+	resp, err := client.FindUsers(SearchRequest{Limit: 1})
+	if err == nil {
+		t.Errorf("Response should return error, [%v]", resp)
+	}
+
+	if !strings.Contains(err.Error(), "unknown error") {
+		t.Errorf("Wrong error type received, [%v]", err)
+	}
+}
+
+func TestFindUsersAuth(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer testServer.Close()
+
+	client := &SearchClient{
+		URL:         testServer.URL,
+		AccessToken: "bad_token",
+	}
+
+	resp, err := client.FindUsers(SearchRequest{Limit: 1})
+	if err == nil {
+		t.Errorf("Response should return error, [%v]", resp)
+	}
+
+	if !strings.Contains(err.Error(), "Bad AccessToken") {
 		t.Errorf("Wrong error type received, [%v]", err)
 	}
 }
@@ -138,6 +372,14 @@ func TestFindUsersGoodParam(t *testing.T) {
 				Offset: 1,
 			},
 			ExpectedTotal: 25,
+		},
+		TestCase{
+			Description: "Offset: 5, Limit: 10",
+			Request: SearchRequest{
+				Limit: 14,
+				Query: "less_data",
+			},
+			ExpectedTotal: 10,
 		},
 	}
 
--- http/parser.go
+++ http/parser.go
@@ -1,54 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"encoding/xml"
 	"fmt"
-	"io"
 	"io/ioutil"
 )
-
-type Root struct {
-	Row []User `xml:"row"`
-}
-
-func decode(data []byte) []User {
-	input := bytes.NewReader(data)
-	decoder := xml.NewDecoder(input)
-
-	var users []User
-	for {
-		token, tokenErr := decoder.Token()
-		if tokenErr == io.EOF {
-			break
-		}
-
-		if tokenErr != nil {
-			fmt.Println("error happend", tokenErr)
-			break
-		}
-
-		if token == nil {
-			fmt.Println("t is nil break")
-		}
-
-		switch token := token.(type) {
-		case xml.StartElement:
-			if token.Name.Local == "row" {
-				var user User
-				err := decoder.DecodeElement(&user, &token)
-				if err != nil {
-					fmt.Println("error happend", err)
-				}
-				user.Name = user.FirstName + " " + user.LastName
-
-				users = append(users, user)
-			}
-		}
-	}
-
-	return users
-}
 
 func main() {
 	data, err := ioutil.ReadFile("dataset.xml")
